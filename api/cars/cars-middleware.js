85914bef656a4572e332b6714b116a1ba17397a8
Michael-Kochis <rhishisikk@gmail.com>
2021-07-12 16:51:08 -0500

    correcting spelling errors in read.me

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,16 @@
+const cars = require('./cars-model')
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.body;
+
+  cars.getById(id)
+    .then((resp) => {
+      if (!resp || resp === undefined || resp === null) {
+        res.status(404).json({ message: `car with id ${id} is not found` })
+      } else {
+        next();
+      }
+    }).catch(next);
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +23,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid
 }
