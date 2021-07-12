55fa62771ed61cc2a768058ba0df14764907fda3
Michael-Kochis <rhishisikk@gmail.com>
2021-07-12 16:41:00 -0500

    cars model initial pass

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,16 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  db('cars');
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = (id) => {
+  db('cars')
+    .where( { id })
+    .first();
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = (neoCar) => {
+  db('cars')
+    .insert(neoCar);
 }
