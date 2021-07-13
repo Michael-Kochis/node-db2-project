88338d7640ef2644f69c1a49fd7b3910993e53b8
Michael-Kochis <rhishisikk@gmail.com>
2021-07-13 10:23:51 -0500

    initial pass - migration

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.bigInteger('id').unsigned().notNullable();
+    table.text('vin', 64).notNullable();
+    table.text('make', 64).notNullable();
+    table.text('model', 64).notNullable();
+    table.bigInteger('mileage').unsigned().notNullable();
+    table.text('title', 256);
+    table.text('transmission', 256);
+    table.unique('id');
+    table.unique('vin');
+})
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
