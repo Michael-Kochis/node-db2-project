40468c5fb9afb436033934b9b686bf3fdb774d90
Michael-Kochis <rhishisikk@gmail.com>
2021-07-13 11:03:43 -0500

    initial database migration, still need seed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', table => {
     table.bigInteger('id').unsigned().notNullable();
-    table.text('vin', 64).notNullable();
+    table.text('vin', 64).notNullable().index();
     table.text('make', 64).notNullable();
     table.text('model', 64).notNullable();
     table.bigInteger('mileage').unsigned().notNullable();
